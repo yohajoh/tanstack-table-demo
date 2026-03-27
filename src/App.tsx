56bdf2437fe31bc8d1d47d97b970a9057c89820e
yohajoh <ybelete490@gmail.com>
2026-03-27 13:57:21 -0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,13 @@
+import { useReactTable } from "@tanstack/react-table";
+
+const options = {
+  columns: "",
+  data: "",
+  getCoreRowModel: {},
+};
+
 function App() {
+  // const table = useReactTable(options);
   return (
     <div className="max-w-200 mt-3 mx-auto">
       <h1 className="font-bold text-3xl text-brand">Well come to tanstack table</h1>
